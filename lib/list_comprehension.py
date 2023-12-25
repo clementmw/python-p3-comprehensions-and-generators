dada185eb7ac49d3779526bddfa3a795976f2c6a
Clement Macharia <clementmacharia62@gmail.com>
2023-12-26 00:51:49 +0300

    complete

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_list = [i for i in num_list  if i%2 == 0 ]
+    return even_list
+num_list = [0,1,3,5,7,8,9]
+result = return_evens(num_list)
+print(result)
+
+    
 
 def make_exclamation(sentence_list):
-    pass
+    exclamation = [i + "!" for i in sentence_list]
+    return exclamation
+sentence_list = ["Hello", "I'm doing great", "Python is fun"]
+result = make_exclamation(sentence_list)
+print(result)
